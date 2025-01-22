4fdebb2770a7726c5dffc9ece497af7b12340602
Sajid <sajidahamed2001@gmail.com>
2025-01-22 17:15:11 +0530

    testing configured

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+// This will be your main entry point
+// We can leave it minimal for now while focusing on the ship factory 
---
+++ tests/ship.test.js
@@ -0,0 +1,8 @@
+import { ship } from '../src/factories/ship';
+
+describe('demo', () => {
+    test('demo', () => {
+        expect(ship()).toBe(1);
+    });
+});
+
