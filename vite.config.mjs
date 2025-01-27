b39a9f6a267f3acb3da25aefe706207bc4c57208
Sajid <sajidahamed2001@gmail.com>
2025-01-27 22:47:39 +0530

    moved html inside src and updated the vite config file for the same

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  }
 })
