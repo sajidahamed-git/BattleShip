f8265f453d8f4831e8da0733dfe8d6a13e7445e9
Sajid <sajidahamed2001@gmail.com>
2025-01-29 19:56:05 +0530

    cleanup

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -5,12 +5,23 @@ import shipPlacer from "../controllers/shipPlacer";
 
 const game = (() => {
   
-  // Initialize players
   const computer = Player("Computer");
   const computerBoard = computer.gameBoard;
-
+  
   const gameMessage = document.getElementById("game-message");
 
+  const initialize = () => {
+    const playerBoardElement = document.getElementById("player-board");
+    const computerBoardElement = document.getElementById("computer-board");
+  
+    // Create board cells
+    createBoardCells(playerBoardElement,"player");
+    createBoardCells(computerBoardElement,"computer");
+    shipPlacer.placeComputerShips(computerBoard);  // Pass the computerBoard
+    // Set initial message with length
+    gameMessage.textContent = `Place your carrier (length: 6)`;
+  };
+  
   const playerAttack = (i,j) => {
     if (computerBoard.board[i][j] !== null) {
       updateColor(i,j,'hit');
@@ -18,21 +29,6 @@ const game = (() => {
       updateColor(i,j,'miss');
     }
   }
-
-  // Initialize the game
-  const initialize = () => {
-    const playerBoardElement = document.getElementById("player-board");
-    const computerBoardElement = document.getElementById("computer-board");
-
-    // Create board cells
-    createBoardCells(playerBoardElement,"player");
-    createBoardCells(computerBoardElement,"computer");
-    shipPlacer.placeComputerShips(computerBoard);  // Pass the computerBoard
-
-    // Set initial message with length
-    gameMessage.textContent = `Place your carrier (length: 6)`;
-  };
-
 
   return {
     initialize,
