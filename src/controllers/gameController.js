03be5dee1777c33f92e2daca0594a07cc84c5131
Sajid <sajidahamed2001@gmail.com>
2025-01-29 19:48:37 +0530

    cleanup

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,5 +1,4 @@
 import Player from "../factories/playerFactory";
-import createShip from "../factories/shipFactory";
 import createBoardCells from "./domController";
 import {updateColor} from "./domController";
 import shipPlacer from "../controllers/shipPlacer";
@@ -7,50 +6,10 @@ import shipPlacer from "../controllers/shipPlacer";
 const game = (() => {
   
   // Initialize players
-  // const player = Player("Player 1");
   const computer = Player("Computer");
-  const directionButton = document.getElementById("flip-ship-direction");
-  const startButton = document.getElementById("start-button");
-  const gameMessage = document.getElementById("game-message");
-
-  // Initialize game boards in memory in form of 2d array
-  // const playerBoard = player.gameBoard;
   const computerBoard = computer.gameBoard;
-  // Ships to be placed
-  const ships = [
-    { length: 6, name: "Carrier" },  
-    { length: 5, name: "Battleship" },
-    { length: 4, name: "Cruiser" },
-    { length: 3, name: "Submarine" },
-    { length: 2, name: "Destroyer" },
-  ];
 
-  let currentShipIndex = 0;
-  let currentDirection = "horizontal"; // default direction
-
-  // const placePlayerShip = (row, col) => {
-  //   const ship = createShip(ships[currentShipIndex].length);
-  //   //todo :add some validation here when user clicks on cell after placing all ships
-  //   //ie currentShipIndex is greater than ships.length
-  //   try {
-  //     playerBoard.validateAndPlaceShip(ship, row, col, currentDirection);
-  //     playerBoard.updateBoardDisplay();
-  //     currentShipIndex++;
-  //     // console.log(playerBoard.board);
-
-  //     // Update game message
-  //     if (currentShipIndex < ships.length) {
-  //       gameMessage.textContent = `Place your ${ships[currentShipIndex].name} (length: ${ships[currentShipIndex].length})`;
-  //     } else {
-  //       gameMessage.textContent = "All ships placed!";
-  //       directionButton.classList.add("hidden");
-  //       startButton.classList.remove("hidden");
-  //     }
-  //   } catch (error) {
-  //     gameMessage.textContent = error.message;
-  //     console.error("Invalid placement:", error);
-  //   }
-  // };
+  const gameMessage = document.getElementById("game-message");
 
   const playerAttack = (i,j) => {
     if (computerBoard.board[i][j] !== null) {
@@ -74,16 +33,9 @@ const game = (() => {
     gameMessage.textContent = `Place your carrier (length: 6)`;
   };
 
-  const flipShipDirection = () => {
-    currentDirection =
-      currentDirection === "horizontal" ? "vertical" : "horizontal";
-    directionButton.textContent = `current direction: ${currentDirection} click to flip`;
-  };
 
   return {
     initialize,
-    flipShipDirection,
-    // placePlayerShip,
     playerAttack,
   };
 })();
